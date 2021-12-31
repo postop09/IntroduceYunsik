2666ad3456f2fd52cdf81404bb5b37d88003f5a9
YUNSIK.CHO <postop09@naver.com>
2021-12-31 21:14:27 +0900

    mode black&white

--- js/introduce.js
+++ js/introduce.js
@@ -3,13 +3,14 @@ const myself = document.querySelectorAll('.artic_myself');
 // 전체에 적용 시켜야 한다.
 // forEach를 사용하는 방법 - 해결
 // jQuery를 사용하는 방법
-let username = prompt('사용자 이름을 입력해주세요.')
+let username = prompt('사용자 이름을 입력해주세요.');
+alert(` ${username}님, 환영합니다! 응원의 메세지를 남겨주세요.`);
 
 myself.forEach((i) => {
   const btnL = i.querySelector('.btn_lSlide');
   const btnR = i.querySelector('.btn_rSlide');
   const imgMe = i.querySelector('.list_img');
-  const imgLenght = imgMe.children.length
+  const imgLenght = imgMe.children.length;
   let moveX = 0;
 
   // 예외처리 : 이미지가 없는 곳으로 넘어가면 안됌.
@@ -18,52 +19,28 @@ myself.forEach((i) => {
     if(moveX > (imgLenght - 1) * (-600)) {
       moveX += -600;
     }
-    imgMe.style.transform = `translate(${moveX}px, 0)`
+    imgMe.style.transform = `translate(${moveX}px, 0)`;
   })
   btnL.addEventListener('click', () => {
     if(moveX < 0) {
       moveX += 600;
     }
-    imgMe.style.transform = `translate(${moveX}px, 0)`
+    imgMe.style.transform = `translate(${moveX}px, 0)`;
   })
 })
 
-// function imgSlide() {
-//   const btnL = myself.querySelector('.btn_lSlide');
-//   const btnR = myself.querySelector('.btn_rSlide');
-//   const imgMe = myself.querySelector('.list_img');
-//   const imgLenght = imgMe.children.length
-//   let moveX = 0;
-
-//   // 예외처리 : 이미지가 없는 곳으로 넘어가면 안됌.
-//   imgMe.style.width = `${imgLenght} * 600px`
-//   btnR.addEventListener('click', () => {
-//     if(moveX > (imgLenght - 1) * (-600)) {
-//       moveX += -600;
-//     }
-//     imgMe.style.transform = `translate(${moveX}px, 0)`
-//   })
-//   btnL.addEventListener('click', () => {
-//     if(moveX < 0) {
-//       moveX += 600;
-//     }
-//     imgMe.style.transform = `translate(${moveX}px, 0)`
-//   })
-// }
-// imgSlide()
-
 // 좋아요
 myself.forEach((i) => {
-  const like = i.querySelector('.output_like') 
+  const like = i.querySelector('.output_like');
   const btnHeart = i.querySelector('.btn_heart');
   let count = 0;
   
   btnHeart.addEventListener('click', () => {
     btnHeart.classList.toggle('on');
     if(btnHeart.className == 'btn_heart on') {
-      like.textContent = ++count
+      like.textContent = ++count;
     } else if(btnHeart.className == 'btn_heart') {
-      like.textContent = --count
+      like.textContent = --count;
     }
   })
 })
@@ -98,7 +75,7 @@ myself.forEach((i) => {
   
     // 예외처리 : 아무것도 입력이 안되면 댓글 작성x
     if(inpComment.value == '') {
-      alert('내용을 작성해주세요.')
+      alert('내용을 작성해주세요.');
       liTag.innerHTML = '';
     } else if(inpComment.value != '') {
       liTag.innerHTML = `
